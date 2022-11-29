c5cc9bee5d0ed69e82ad15e1f25c9acbe9ac05c7
Holedesu <88774183+Holedesu@users.noreply.github.com>
2022-11-29 14:08:39 +0300

    Ended third step

---
+++ src/cli.js
@@ -0,0 +1,12 @@
+// Import
+import readlineSync from "readline-sync";
+
+// Code
+
+export default ()=> {
+
+    console.log("Welcome to the Brain Games!")
+
+    const name = readlineSync.question('May I have your name? ');
+    console.log("Hello, " + name + "!")
+}
