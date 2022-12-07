5f2b38fe939b6dff371e99e11cf5a640070fe7e8
Holedesu <88774183+Holedesu@users.noreply.github.com>
2022-12-08 00:15:00 +0300

    Fix for game-prime #2

--- src/prime.js
+++ src/prime.js
@@ -19,11 +19,11 @@ export default () => {
     if ((isPrime(generator1) === true && answer === 'yes') || (isPrime(generator1) === false && answer === 'no')) {
       console.log('Correct!');
     } else if (isPrime(generator1) === true && answer === 'no') {
-      return console.log('"no" is wrong answer ;(. Correct answer was "yes"');
+      return console.log(`"no" is wrong answer ;(. Correct answer was "yes"\nLet's try again, ${name}!`);
     } else if (isPrime(generator1) === false && answer === 'yes') {
-      return console.log('"yes" is wrong answer ;(. Correct answer was "no"');
+      return console.log(`"no" is wrong answer ;(. Correct answer was "yes"\nLet's try again, ${name}!`);
     } else {
-      return console.log('You should type only yes or no');
+      return console.log(`You should type only "yes" or "no".\nLet's try again, ${name}!`);
     }
 
     i += 1;
